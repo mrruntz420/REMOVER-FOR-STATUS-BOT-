1e793f0d4c1c660484055cc4dbbf2c0773b8c294
mrruntz420 <reyvaughnmoreno83@gmail.com>
2024-11-20 15:56:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🔥GENING ACCOUNTS 24/7 FOR MRRUNTZ420!", "🎮 Playing RUST"];
+const statusMessages = ["Playing RUST"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
